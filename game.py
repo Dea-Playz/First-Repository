3fbf1bd7decdedeba9636bcdc4ca9dbdabb0d8cf
Dea-Playz <144243858+Dea-Playz@users.noreply.github.com>
2023-10-19 18:27:42 +0530

    Passions python game

---
+++ game.py
@@ -0,0 +1,10 @@
+this_is_me = {
+    "name": "Dea",
+    "github-have": True,
+    "passion": ["coding", "drawing"]
+}
+
+print("My name is", this_is_me["name"])
+if this_is_me["github-have"]: print("I have github on my computer")
+
+print("My passions are",this_is_me["passion"][0],"and",this_is_me["passion"][1])
